df193d692bb12957c27bc60216f8755884e05d09
Juma-Lavenda <123526443+Juma-Lavenda@users.noreply.github.com>
2023-03-20 23:42:01 -0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react"
 export default function Navbar(){
         return (
                 <nav>
-                        <img src = "../images/logo.jpg" />
+                        <img src = "/images/logo.jpg" />
                         <h1>todolist</h1>
                 </nav>
         )
